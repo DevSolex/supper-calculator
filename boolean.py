3fe8db1eacd49b4351d25051ec3eeb0dd255818f
DevSolex <devsolex6@gmail.com>
2025-07-16 10:30:01 +0000

    second commit

--- boolean.py
+++ boolean.py
@@ -8,7 +8,7 @@ first_name = "Tom"
 my_favorite_number = 0
 print(bool(first_name))
 print(bool(my_foavorite_number))
-'''
+
 
 firstVariable = " "
 secondVariable = True
@@ -19,4 +19,14 @@ thirdVariable = ""
 forthVariable = False
 print(bool(thirdVariable))
 print(forthVariable)
+'''
 
+number_one = 0
+number_two = ""
+print(bool(number_one))
+print(bool(number_two))
+
+number_three = -1
+number_four = 0.5
+print(bool(number_three))
+print(bool(number_four))
