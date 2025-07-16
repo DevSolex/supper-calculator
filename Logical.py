7acf0194eb27dc59665590744ae607c5ab509c39
DevSolex <devsolex6@gmail.com>
2025-07-16 11:52:31 +0000

    added multiplication and subtraction feature

--- Logical.py
+++ Logical.py
@@ -12,3 +12,26 @@ first_number = input("first number:")
 second_number = input("second number:")
 sum = float(first_number) + float(second_number)
 print(f"{first_number} + {second_number} = {sum}")
+
+print('''***********************
+subtraction
+********************************
+Enter two numbers to subtrac''')
+#prompt a user for a number and store it
+first_number = input("first number:")
+#prompt the user for nuber and store it
+second_number = input("second number:")
+sub = float(first_number) - float(second_number)
+print(f"{first_number} - {second_number} = {sub}")
+
+print('''***********************
+multiplication
+********************************
+Enter two numbers to multiply''')
+#prompt a user for a number and store it
+first_number = input("first number:")
+#prompt the user for nuber and store it
+second_number = input("second number:")
+mul = float(first_number) * float(second_number)
+print(f"{first_number} * {second_number} = {mul}")
+
