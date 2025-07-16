499ec51a856fca2bafff4c876d2370afa66a8a52
DevSolex <devsolex6@gmail.com>
2025-07-16 10:13:59 +0000

    first commit

---
+++ boolean.py
@@ -0,0 +1,22 @@
+'''isMarried = True
+isMarried = False
+print(isMarried)
+
+#Data type
+#print(type(isMarried))
+first_name = "Tom"
+my_favorite_number = 0
+print(bool(first_name))
+print(bool(my_foavorite_number))
+'''
+
+firstVariable = " "
+secondVariable = True
+print(bool(firstVariable))
+print(secondVariable)
+
+thirdVariable = ""
+forthVariable = False
+print(bool(thirdVariable))
+print(forthVariable)
+
